f5bc24a1c1edd92077d70928ae6ec8c15349466e
kkuop <60794860+kkuop@users.noreply.github.com>
2020-03-31 15:18:15 -0500

    added a back-to-top button

--- Client/app.js
+++ Client/app.js
@@ -65,3 +65,20 @@ function displayAddForm() {
 function closeAlert() {
     document.getElementById("response-to-add").classList.add("invis");
 }
+
+var myBtn = document.getElementById("backToTopBtn");
+window.onscroll = function() { scrollFunction()};
+
+function scrollFunction() {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        myBtn.style.display = "block";
+        
+    } else {
+        myBtn.style.display = "none";
+    }
+}
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
